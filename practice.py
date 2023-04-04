8ca6c58c9ce563b43c78d00467ae175eb2a3ad86
Auntor Acharja <acharjeeauntor@gmail.com>
2023-04-04 22:38:16 +0600

    Adding practice file

---
+++ practice.py
@@ -0,0 +1,7 @@
+################################# Variables
+
+patient_name = "Auntor"
+age = 25
+is_new_patient = False
+
+print(f"{patient_name} is {age} years old. Is he is a new patient?: {is_new_patient}")

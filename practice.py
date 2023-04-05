a5caea8c7f05ba9f21374861a1a8db8217cbb1fc
Auntor Acharja <acharjeeauntor@gmail.com>
2023-04-05 10:30:07 +0600

    Practice  receiving input and type conversation

--- practice.py
+++ practice.py
@@ -1,7 +1,55 @@
 ################################# Variables
 
-patient_name = "Auntor"
-age = 25
-is_new_patient = False
 
-print(f"{patient_name} is {age} years old. Is he is a new patient?: {is_new_patient}")
+
+# patient_name = "Auntor"
+# age = 25
+# is_new_patient = False
+
+# print(f"{patient_name} is {age} years old. Is he is a new patient?: {is_new_patient}")
+
+
+################################# Receiving Input
+# Note:
+# input function always return a String.
+
+
+
+
+# name = input("What is you name? ")
+# print('Hello '+name)
+
+
+################################# Type Conversion
+# Note:
+# Those are built-in type conversion function. 
+# int()
+# str()
+# float()
+# bool()
+
+# birth_date = input("What is your birth date? ")
+# age = 2023-int(birth_date)
+# print(f"Your age is: {age}")
+# print(type(age))
+# age = age+0.5
+# print(int(age))
+# age=float(age)
+# print(type(age))
+# print(age)
+# age = str(age)
+# print(type(age))
+# print(age)
+
+# isTrue = "True"
+# print(type(isTrue))
+# isTrue = bool(isTrue)
+# print(isTrue)
+# print(type(isTrue))
+
+
+# Sum Two number
+# first_number = float(input("Enter First  Number: "))
+# second_number = float(input("Enter Second Number: "))
+# sum = first_number + second_number
+# print(sum)

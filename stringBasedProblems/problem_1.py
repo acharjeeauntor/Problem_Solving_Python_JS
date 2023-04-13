b6ca82edc35bd9406e26e8cb636f8675bb667bad
Auntor Acharja <acharjeeauntor@gmail.com>
2023-04-13 08:18:47 +0600

    Seperate folders

---
+++ stringBasedProblems/problem_1.py
@@ -0,0 +1,9 @@
+# Problem 1 : Can you write a method that will erase any character from a string?
+
+def erase_character_from_string(string,char):
+
+
+
+erase_character_from_string("auntor","a")
+
+

2ce6b0b123e8a5e346951b526f05e5877e51c7c8
auntor.acharja <auntor.acharja@brotecs.com>
2023-04-25 16:55:51 +0600

    Solved Problem no. 4

--- stringBasedProblems/problem_4.py
+++ stringBasedProblems/problem_4.py
@@ -1,10 +1,22 @@
 # Problem 4 : How do you find the maximum occurring character in a given string?
 
 text = input("Enter your String: ")
+
+index=0
 count=0
-for x in text:
-    for y in text:
-        if(x==y):
+lastHeighestValue=0
+
+for x in range(0,len(text)):
+    for y in range(x+1,len(text)):
+        if(text[x]==text[y]):
             count+=1
-    print(f"{x} = {count}")
-    count=0
+    if(count>lastHeighestValue):
+        lastHeighestValue=count
+        count=0
+        index=x
+
+if(lastHeighestValue==0):
+    print("There are no maximum occurring character in the given string")
+else:
+ print(f"Maximum occurring character in the given string is {text[index]}")
+

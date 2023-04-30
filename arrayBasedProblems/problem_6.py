05fa43ab7f27734820d48947515a79c964b20d6a
Auntor Acharja <acharjeeauntor@gmail.com>
2023-04-30 21:34:37 +0600

    Solved Problem 6 of Array based problem

---
+++ arrayBasedProblems/problem_6.py
@@ -0,0 +1,9 @@
+# Problem 6 - How do you convert a byte array into a string?
+
+array = ['a','u','n','t','o','r']
+newString=""
+for char in array:
+    newString+=char
+
+print(newString)
+print(type(newString))

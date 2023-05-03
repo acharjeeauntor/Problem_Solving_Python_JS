d4b1c7eab0ad53df526588ed20f06df4192339ae
auntor.acharja <auntor.acharja@brotecs.com>
2023-05-03 09:44:05 +0600

    Solved Pblm 1 of SearchAndSortBasedProblem

--- searchAndSortBasedProblems/problem_1.py
+++ searchAndSortBasedProblems/problem_1.py
@@ -2,11 +2,21 @@
 
 array = [2,4,5,1,6,8,0,3,7]
 
+# First Way
+# for i in range(0,len(array)):
+#     for j in range(i+1,len(array)):
+#         if(array[i]>array[j]):
+#             temp = array[i]
+#             array[i] = array[j]
+#             array[j] = temp
+
+# print(array)
+
+# Second Way
 for j in range(0,len(array)):
     for i in range(1,len(array)):
         if(array[i]<array[i-1]):
             temp = array[i-1]
-            array[i] = array[i-1]
-            array[i-1] = temp
-
+            array[i-1]=array[i]
+            array[i] = temp
 print(array)

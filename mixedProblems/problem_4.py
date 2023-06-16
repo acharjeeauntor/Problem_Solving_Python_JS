46801086b4a9258febd666b5b54a1bbb01a88249
Auntor Acharja <acharjeeauntor@gmail.com>
2023-06-16 10:04:25 +0600

    change factorial concept

--- mixedProblems/problem_4.py
+++ mixedProblems/problem_4.py
@@ -1,10 +1,24 @@
 # Problem 4 : How do you find the factorial of an integer?
 
+# def factorial(n):
+#     if(n==1):
+#         return 1
+#     else:
+#         return (n*factorial(n-1))
+    
+
+# print(factorial(1))
+
+
 def factorial(n):
+    y=1
     if(n==1):
         return 1
     else:
-        return (n*factorial(n-1))
+        for x in range(5,0,-1):
+            y=y*x
+        return y
+
     
 
 print(factorial(1))

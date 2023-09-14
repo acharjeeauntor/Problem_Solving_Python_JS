6aa1182e3ef88261f1ce19aab028f84ae48105e4
Auntor Acharja <auntor.acharja@brotecs.com>
2023-09-14 09:26:50 +0600

    Solved array problem 6 in js

---
+++ arrayBasedProblems/problem_6.js
@@ -0,0 +1,8 @@
+// Problem 6 - How do you convert a byte array into a string?
+
+var array = ['a','u','n','t','o','r']
+var newString=""
+array.forEach(word=>{
+    newString+=word
+})
+console.log(newString)
